76ac6c028e70b57e3e81e484f3c26d59f7934fbe
Xiaoyu Wang <31714478+Rainniee@users.noreply.github.com>
2017-09-30 16:18:42 +0800

    Update Assignments 3.java

--- Assignments 3.java
+++ Assignments 3.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package calculator;
+package Assignments 3;
 import java.util.Scanner;
 
 /**
