f7538260930a4019009d332af3e514282a20e811
Xiaoyu Wang <31714478+Rainniee@users.noreply.github.com>
2017-10-03 09:03:44 +0800

    Add files via upload

---
+++ JavaApplication2.java
@@ -0,0 +1,79 @@
+/*
+ * Now you are given a string S, which represents a software license key which we would like to format. 
+ * The string S is composed of alphanumerical characters and dashes. 
+ * The dashes split the alphanumerical characters within the string into groups. (i.e. if there are M dashes, the string is split into M+1 groups). 
+ * The dashes in the given string are possibly misplaced.
+ * We want each group of characters to be of length K (except for possibly the first group, which could be shorter, but still must contain at least one character). 
+ * To satisfy this requirement, we will reinsert dashes. Additionally, all the lower case letters in the string must be converted to upper case.
+ * So, you are given a non-empty string S, representing a license key to format, and an integer K. And you need to return the license key formatted according to the description above.
+ */
+
+/** My thought:
+ * 1.Because of the dashes, we cannot find out the correct length of the initial String S.So I firstly remove the dashes and converted letters to upper case.
+ * 2.After getting the newLength,I create an array and make it format.
+ */
+
+package javaapplication2;
+import java.util.Scanner;
+
+/**
+ *
+ * @author apple
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter a string S: ");
+        String s = in.next();
+        System.out.println("Please enter a positive number K: ");
+	int k = in.nextInt();
+        licenseKeyFormatting(s,k);
+// TODO code application logic here
+    }
+    
+    public static String licenseKeyFormatting(String S, int K) {
+        int len = 0;
+        int n = 'a' - 'A';
+        char[] tempArray = S.toCharArray(); //converts String S to a new character String.
+        for (int i = 0; i < tempArray.length; i++) {
+            char ch = tempArray[i];
+            if (ch == '-') {
+                continue;
+            }
+            else if (ch >= 'a' && ch <= 'z') {
+                tempArray[i] -= n;
+            }
+            tempArray[len] = tempArray[i];
+            len++;
+        }
+        int newLength;
+        if (len % K == 0){
+            newLength = len + len / K - 1;
+        }
+        else {
+            newLength = len + len / K;
+        }
+        char[] newArray = new char[newLength];
+        int m = len;
+        for (int i = newLength - 1; i >= 0; i--) {
+            if (i != newLength - 1 && (newLength-i) % (K+1) == 0) {
+                newArray[i] = '-';
+            } 
+            else {
+                m--;
+                newArray[i] = tempArray[m];
+            }
+        }
+        System.out.println(newArray);
+        return String.valueOf(newArray);
+      
+    }
+}
+
+
+
+
