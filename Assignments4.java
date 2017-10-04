7658a860fa63ef53151a4fb1be69e42751ed80e5
Xiaoyu Wang <31714478+Rainniee@users.noreply.github.com>
2017-10-04 16:33:19 +0800

    Update Assignments4.java

--- Assignments4.java
+++ Assignments4.java
@@ -75,5 +75,117 @@ public class Assignments4 {
 }
 
 
+/* Question 2
+ * Implement a class called Tool. It should have an int field called strength and a char field called type.You may make them either private or protected. 
+ * The Tool class should also contain the function void setStrength(int), which sets the strength for the Tool.
+ * Create 3 more classes called Rock, Paper, and Scissors, which inherit fromc Tool. 
+ * Each of these classes will need a constructor which will take in an int that is used to initialise the strength field. 
+ * The constructor should also initialise the type field using 'r' for Rock, 'p' for Paper, and 's' for Scissors.
+ * These classes will also need a public function bool fight(Tool) that compares their strengths in the following way:
+ * Rock's strength is doubled (temporarily) when fighting scissors, but halved (temporarily) when fighting paper. 
+ * In the same way, paper has the advantage against rock, and scissors against paper. 
+ * The strength field shouldn't change in the function, which returns true if the original class wins in strength and false otherwise. 
+ * You may also include any extra auxiliary functions and/or fields in any of these classes. 
+ * Run the program without changing the main function, and verify that the results are correct. (Score 2)
+ */
+
+/**
+ *
+ * @author apple
+ */
+
+class Tool{
+    protected int strength;
+    protected char type;
+        
+    public Tool(){
+        
+    }
+   
+    public Tool(int n){
+        this.strength = n;
+    }
+    public Tool(int n, char c){
+        this.strength = n;
+        this.type = c;
+    }
+        
+    public void setStrength(int n){
+        this.strength = n;
+    }
+    public int getStrength(){
+        return this.strength;
+    }
+    public void setType(char c){
+        this.type = c;
+    }
+    public char getType(){
+        return this.type;
+    }
+}
+
+class Scissors extends Tool {
+        public Scissors(){
+            this.type = 's';
+        }
+        public Scissors(int n){
+            this.strength = n;
+            this.type = 's';
+        }
+        public boolean fight(Tool tool){
+            if(tool.type == 'r'){
+                return this.strength > tool.strength * 2;
+            }
+            else if(tool.type == 'p'){
+                return this.strength * 2 > tool.strength;
+            }
+            else
+                return this.strength > tool.strength;
+        }
+    }
+}
+
+class Paper extends Tool {
+        public Paper(){
+            this.type = 'p';
+        }
+        public Paper(int n){
+            this.strength = n;
+            this.type = 'p';
+        }
+        public boolean fight (Tool tool){
+            if(tool.type == 'r'){
+                return this.strength * 2 > tool.strength;
+            }
+            else if(tool.type == 's'){
+                return this.strength > tool.strength * 2;
+            }
+            else
+                return this.strength > tool.strength;
+        }
+    }
+}
+
+class Rock extends Tool{
+        public Rock(){
+            this.type = 'r';
+        }
+        public Rock(int n){
+            this.strength = n;
+            this.type = 'r';
+        }
+        public boolean fight (Tool tool){
+            if(tool.type == 'p'){
+                return this.strength > tool.strength * 2;
+            }
+            else if(tool.type == 's'){
+                return this.strength * 2 > tool.strength;
+            }
+            else
+                return this.strength > tool.strength;
+        }
+    }
+}
+
 
 
