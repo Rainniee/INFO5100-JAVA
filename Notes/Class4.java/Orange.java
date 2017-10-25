915241f27da24a3c2bf36415ff86e396ec97b0cb
Xiaoyu Wang <31714478+Rainniee@users.noreply.github.com>
2017-10-25 16:14:01 +0800

    Update Orange.java

--- Notes/Class4.java/Orange.java
+++ Notes/Class4.java/Orange.java
@@ -3,12 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package class3;
+package class4;
 
-/**
- *
- * @author apple
- */
 public class Orange extends Fruit{
     
     private float juiceLevels;
