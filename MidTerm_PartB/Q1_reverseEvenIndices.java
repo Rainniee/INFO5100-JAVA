492761b63f2fb51a682d8257886eaa2cb901bec8
Xiaoyu Wang <31714478+Rainniee@users.noreply.github.com>
2017-10-26 19:24:58 +0800

    Finished Q1_reverseEvenIndices

--- MidTerm_PartB/Q1_reverseEvenIndices.java
+++ MidTerm_PartB/Q1_reverseEvenIndices.java
@@ -1,16 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package midtermpartb;
+/**
+ * Q1
+ * Write a method named reverseEvenIndices that takes an integer array asinput and outputs an array 
+ * such that all the values with odd indices remain in the same position
+ * However, elements with even indices should be output in reverse order
+ * That is, the first element with even index should be swapped with the last element with even index
+ * the second een-indexed element with the second-to-last even-indexed element, and so on
+ **/
 
-import java.util.Arrays;
+package midtermpartb;
 
 /**
  *
- * @author Administrator
+ * @author Apple
  */
+
 public class reverseEvenIndices {
     public static int[] reverseEvenIndices(int[] nums) {
         int n = nums.length;
