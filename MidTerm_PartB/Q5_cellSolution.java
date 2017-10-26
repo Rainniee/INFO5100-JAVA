737a27e538bc8e053717257daaa7deebca240533
Xiaoyu Wang <31714478+Rainniee@users.noreply.github.com>
2017-10-26 19:41:26 +0800

    Finished Extra Q5

--- MidTerm_PartB/Q5_cellSolution.java
+++ MidTerm_PartB/Q5_cellSolution.java
@@ -1,16 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/**
+ * Q5
+ * A Maze is given as N*N binary matrix of blocks where source block is the upper left most block 
+ * i.e., maze[0][0] and destination block is lower rightmost block i.e., maze[N-1][N-1]
+ * A rat starts from source and has to reach destination
+ * The rat can move only in two directions: forward and down
+ * In the maze matrix, 0 means the block is dead end and 1 means the block can be used in the path from source to destination
+ * Your function should take the maze as an input and return an ArrayList of the resulting path
+ **/
+
 package midtermpartb;
 
 import java.util.ArrayList;
 
 /**
  *
- * @author Administrator
+ * @author Apple
  */
+
 class Cell{
     int x,y;
     Cell(int x, int y){
