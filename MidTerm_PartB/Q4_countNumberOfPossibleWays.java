f529984ed5790b9fed010ae209a21acd59ed1a0c
Xiaoyu Wang <31714478+Rainniee@users.noreply.github.com>
2017-10-26 19:37:08 +0800

    Finished Q4_countNumberOfPossibleWays

---
+++ MidTerm_PartB/Q4_countNumberOfPossibleWays.java
@@ -0,0 +1,28 @@
+/**
+ * Q4
+ * Given n dice each with m faces, numbered from 1 to m
+ * find the number of ways to get sum X
+ * X is the summation of values on each facewhen all the dice are thrown
+ * Your function should take, number of faces, number of dice and required sum as input and return a number of possible ways
+ **/
+
+package midtermpartb;
+
+/**
+ *
+ * @author Apple
+ */
+
+public class countNumberOfPossibleWays {
+    
+    public static int countNumberOfPossibleWays(int m, int n, int x) {
+        if (n<1 && m<4) return 0;  
+        if (n==1 && x<=m) return 1;  
+        int value = 0;
+        for (int i=1;i<=m;++i) {
+            value+=countNumberOfPossibleWays(m,n-1,x-i);
+        }
+        return value;
+    }
+    
+}
