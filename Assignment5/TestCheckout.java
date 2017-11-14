89ead2d41a3492c59b5bbd1468754d37b7533c45
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-11-13 19:34:49 -0800

    score update

--- Assignment5/TestCheckout.java
+++ Assignment5/TestCheckout.java
@@ -1,3 +1,6 @@
+/* Good work
+ * Score 7 + extra credit 1; Total score 8
+ */
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
