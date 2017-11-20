ed5cea81ac65b53127fe5f3e44a3175624303483
Xiaoyu Wang <31714478+Rainniee@users.noreply.github.com>
2017-11-20 11:13:17 -0800

    Add files via upload

---
+++ Assignment8/MyJson.java
@@ -0,0 +1,71 @@
+// Q3
+
+package assignment8;
+
+/**
+ *
+ * @author apple
+ */
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MyJson {
+
+    private static ArrayList<Vehicle> readAndGetVehicles(File file) throws IOException {
+        FileReader reader = new FileReader(file);
+        Scanner sc = new Scanner(reader);
+        ArrayList<Vehicle> vehicles = new ArrayList<>();
+        String[] record;
+
+        sc.nextLine(); 
+        while (sc.hasNextLine()) {
+            record = sc.nextLine().split("~");
+            Vehicle vehicle = new Vehicle(record);
+            vehicles.add(vehicle);
+        }
+        reader.close();
+
+        return vehicles;
+    }
+
+    public static String getJsonString(ArrayList<Vehicle> vehicles) {
+        String json = "{\n\"" + vehicles.get(0).webId + "\" : [\n";
+        for (Vehicle vehicle : vehicles) {
+            json += "{\n";
+            json += "\"id\" : \"" + vehicle.id + "\"\n";
+            json += "\"category\" : \"" + vehicle.category + "\"\n";
+            json += "\"year\" : \"" + vehicle.year + "\"\n";
+            json += "\"make\" : \"" + vehicle.make + "\"\n";
+            json += "\"model\" : \"" + vehicle.model + "\"\n";
+            json += "\"trim\" : \"" + vehicle.trim + "\"\n";
+            json += "\"type\" : \"" + vehicle.type + "\"\n";
+            json += "\"price\" : \"" + vehicle.price + "\"\n";
+            json += "\"photo\" : \"" + vehicle.photo + "\"\n";
+            json += "},\n";
+        }
+        json += "]\n}";
+
+        return json;
+    }
+
+    public static void writeToFile(String inputToWrite, String filePath) throws IOException{
+        File f = new File (filePath + "\\Question3_output.txt");
+	f.createNewFile();
+	FileWriter fw = new FileWriter(f);
+	BufferedWriter bf = new BufferedWriter(fw);
+	bf.write(inputToWrite);
+	bf.close();
+    }
+    
+    public static void main(String[] args) throws IOException {
+        File file = new File("C:\\Users\\apple\\Documents\\NetBeansProjects\\Assignment8\\src\\assignment8\\Question3_camino.txt");
+        ArrayList<Vehicle> vehicles = readAndGetVehicles(file);
+        String s = getJsonString(vehicles);
+        writeToFile(s, file.getParent());
+        file = new File("camino_output.txt");
+        // writeTextFromJson(file);
+    }
+    
+}
