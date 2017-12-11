f3479db371880df67cc45d4fa540e2a7ef2d132a
Harsha Vykunta <harshavardhan.biit@gmail.com>
2017-12-11 15:49:08 -0800

    score update

--- Assignment 6/ATM/Bank.java
+++ Assignment 6/ATM/Bank.java
@@ -1,3 +1,6 @@
+/* Awesome work
+ * Total score 10
+ */
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
